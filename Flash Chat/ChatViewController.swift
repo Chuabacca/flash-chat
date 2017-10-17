8a072c0ead7e65b4a8dbc4e4a834e07e1fca406e
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-10-17 16:26:24 -0700

    Wrote code to create table cells and set the message body text with fixture data.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
 
@@ -26,8 +26,9 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        messageTextfield.delegate = self
         
         //TODO: Set yourself as the delegate of the text field here:
 
@@ -38,46 +39,65 @@ class ChatViewController: UIViewController {
         
 
         //TODO: Register your MessageCell.xib file here:
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
 
-        
+        configureTableView()
     }
 
     ///////////////////////////////////////////
     
     //MARK: - TableView DataSource Methods
-    
-    
+
     
     //TODO: Declare cellForRowAtIndexPath here:
-    
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+
+        let messageArray = ["First Message", "Second Message", "Third Message"]
+
+        cell.messageBody.text = messageArray[indexPath.row]
+
+        return cell
+        
+    }
     
     //TODO: Declare numberOfRowsInSection here:
-    
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
     //TODO: Declare tableViewTapped here:
     
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     ///////////////////////////////////////////
     
     //MARK:- TextField Delegate Methods
-    
-    
+
 
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 320
+            self.view.layoutIfNeeded()
+        }
+    }
     
     
     //TODO: Declare textFieldDidEndEditing here:
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+
+    }
 
     
     ///////////////////////////////////////////
--- Flash Chat/Message.swift
+++ Flash Chat/Message.swift
@@ -7,7 +7,7 @@
 class Message {
     
     //TODO: Messages need a messageBody and a sender variable
-    
-    
+    var sender = ""
+    var messageBody = ""
     
 }
